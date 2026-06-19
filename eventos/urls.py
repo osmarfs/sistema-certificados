61ced416c1d354931e53a2a1cb7cc59835ecef50
osmarfs <tshang69@gmail.com>
2026-06-19 01:41:37 -0300

    Adicionado Bootstrap para estilização da lista de eventos

--- eventos/urls.py
+++ eventos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', view.lista_eventos, name='lista_eventos'),
+    path('', views.lista_eventos, name='lista_eventos'),
 ]
